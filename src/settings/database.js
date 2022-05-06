3e3d9d2f7fcb70025be2984b88bbc1959b32597c
jesus1811 <jayarza1811@gmail.com>
2022-05-05 22:32:45 -0500

    deploy production

--- src/settings/database.js
+++ src/settings/database.js
@@ -1,10 +1,10 @@
 import { createConnection } from "mysql";
 
 const connection = createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bd_montalvo",
+  host: "b8tnmbvalnva3ejnhh47-mysql.services.clever-cloud.com",
+  user: "uh2jyy14bev02m6e",
+  password: "CF5NfhRm6XjjZoqOQadt",
+  database: "b8tnmbvalnva3ejnhh47",
 });
 connection.connect((err) => {
   if (err) throw err;
