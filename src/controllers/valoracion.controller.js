f857ab9c44e549fe6c24264c01b6fe6cbe8f7398
jesus1811 <jayarza1811@gmail.com>
2022-06-02 12:37:16 -0500

    add get valoraciones

--- src/controllers/valoracion.controller.js
+++ src/controllers/valoracion.controller.js
@@ -6,3 +6,9 @@ export const postValoracion = (req, res) => {
     err ? res.json(err) : res.json("Agregado Correctamente");
   });
 };
+export const getValoraciones = (req, res) => {
+  const { id } = req.params;
+  connection.query("CALL SP_listar_valoracion(?)", [id], (err, rows) => {
+    err ? res.json(err) : res.json(rows[0]);
+  });
+};
--- src/routers/valoracion.routes.js
+++ src/routers/valoracion.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { postValoracion } from "../controllers/valoracion.controller.js";
+import { getValoraciones, postValoracion } from "../controllers/valoracion.controller.js";
 const router = Router();
 
 router.post("/api/valoracion", postValoracion);
+router.get("/api/valoraciones/:id", getValoraciones);
 
 export default router;
