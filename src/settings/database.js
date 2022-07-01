d6c616584456ddaac47c98cc3a7fc95b77a58ebe
jesus1811 <jayarza1811@gmail.com>
2022-07-01 14:27:04 -0500

    update bd

--- src/settings/database.js
+++ src/settings/database.js
@@ -1,11 +1,11 @@
 import { createConnection } from "mysql2";
 
 const connection = createConnection({
-  host: "containers-us-west-69.railway.app",
+  host: "containers-us-west-54.railway.app",
   user: "root",
-  password: "iwX0LMbBav8DvM3cPWae",
-  database: "railway",
-  port: 6153,
+  password: "Ld16knZOyYN7ICv17gwB",
+  database: "b8tnmbvalnva3ejnhh47",
+  port: 7328,
 });
 connection.connect((err) => {
   if (err) throw err;
